73f7ab5e2e1cb36a109492b96757721a9a078f48
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-12-26 23:55:58 +1100

    Revert "make tracking work on client side"
    This reverts commit 12b666f066ad62752574f62c2c32e30f65ac97d8.

--- components/tracking.jsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import { builder } from '@builder.io/sdk'
-
-builder.trackConversion();
-
-export const Tracking = () => {<></>}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
 import Head from 'next/head'
 import { builder, BuilderComponent } from '@builder.io/react'
-import dynamic from 'next/dynamic'
 
 const BUILDER_API_KEY = process.env.BUILDER_API_KEY;
 builder.init(BUILDER_API_KEY);
-
-const DynamicComponent = dynamic(() =>
-  import('../components/tracking').then((mod) => mod.Tracking),
-  { ssr: false }
-)
+builder.trackConversion();
 
 export const getServerSideProps = async ({res, req, asPath}) => {
   const response = await builder.get('page', { req, res, url: asPath  }).promise();
@@ -27,7 +22,6 @@ export default function Home({builderPage}) {
         <title>Hashify - Share Sensitive Data Securely</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <DynamicComponent />
       <BuilderComponent model="page" content={builderPage} />
     </>
   )
